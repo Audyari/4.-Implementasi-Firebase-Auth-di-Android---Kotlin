93b2bc0378debed184857a5f74da6bbdeb8845e7
Audyari WIyono <Audy123ari@gmail.com>
2025-04-04 18:21:06 +0700

    Membuat Splash Screen

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 "# 4.-Implementasi-Firebase-Auth-di-Android---Kotlin" 
+
+Project:
+
+https://github.com/rhmn68/codepolitan-android-firebase-auth
+
+Docs Firebase Authentication:
+
+https://firebase.google.com/docs/auth
+
+Figma :
+
+https://www.figma.com/file/GMcZDvaukVbbdxSsssrmKn/Untitled?type=design&node-id=0%3A1&mode=design&t=G0CXjlYacIpTx7JD-1
--- app/src/main/java/com/example/firebaseauth/SplashActivity.kt
+++ app/src/main/java/com/example/firebaseauth/SplashActivity.kt
@@ -1,15 +1,17 @@
 package com.example.firebaseauth
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
     }
 }
