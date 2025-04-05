eceece844738c8c4b34490a23c26da479c633722
Audyari WIyono <Audy123ari@gmail.com>
2025-04-05 11:00:52 +0700

    registrasi done

--- app/src/main/java/com/example/firebaseauth/SignUpActivity.kt
+++ app/src/main/java/com/example/firebaseauth/SignUpActivity.kt
@@ -4,8 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -13,6 +16,11 @@ class SignUpActivity : AppCompatActivity() {
     //4. inisilisasi fungsi button
     private lateinit var btnSignUp: Button
     private lateinit var tbSignUp: androidx.appcompat.widget.Toolbar
+
+    //6. inisilisasi fungsi edit text
+    private lateinit var etEmailSignUp: EditText
+    private lateinit var etPasswordSignUp: EditText
+    private lateinit var etConfirmPasswordSignUp: EditText
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +32,36 @@ class SignUpActivity : AppCompatActivity() {
         btnSignUp = findViewById(R.id.btnSignUp)
         tbSignUp = findViewById(R.id.tbSignUp)
 
+        //6. inisialisai lagi email, password confirm password
+
+        etEmailSignUp = findViewById(R.id.etEmailSignUp)
+        etPasswordSignUp = findViewById(R.id.etPasswordSignUp)
+        etConfirmPasswordSignUp = findViewById(R.id.etConfirmPasswordSignUp)
+
+
+
         //1. bikin fungsi action bar
         initActionBar()
 
         //3. panggil fungsi button
         btnSignUp.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+            //6. panggil fungsi edit text
+            val email = etEmailSignUp.text.toString().trim()
+            val pass = etPasswordSignUp.text.toString().trim()
+            val confirmPass = etConfirmPasswordSignUp.text.toString().trim()
+
+
+            //7. panggil fungsi Validasi input
+            if (!isValidInput(email, pass, confirmPass)) return@setOnClickListener
+
+            //8. panggil fungsi register ke firebase
+            // Registrasi ke server
+            registerToServer(email, pass)
+
+
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
         }
 
 //        //5. panggil fungsi toolbar signup
@@ -48,4 +79,50 @@ class SignUpActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = ""
     }
+
+    //7. panggil fungsi validasi input
+    private fun isValidInput(email: String, pass: String, confirmPass: String): Boolean {
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmailSignUp.error = "Invalid Email Format"
+            return false
+        }
+
+        if (pass.length < 6) {
+            etPasswordSignUp.error = "Password must be at least 6 characters"
+            return false
+        }
+
+        if (pass != confirmPass) {
+            etConfirmPasswordSignUp.error = "Passwords do not match"
+            return false
+        }
+        //9. panggil fungsi loading
+//        CustomDialog.hideLoading()
+
+        return true
+    }
+
+    //8. panggil fungsi register ke firebase
+    private fun registerToServer(email: String, pass: String) {
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener { task ->
+
+                //9. panggil fungsi loading
+//                CustomDialog.hideLoading()
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                } else {
+                    Toast.makeText(this, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                //9. panggil fungsi loading
+//                CustomDialog.hideLoading()
+                Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+
 }
