20f48eb8c27e89798d7a395c8870e5ae654e939d
Audyari WIyono <Audy123ari@gmail.com>
2025-04-05 13:43:42 +0700

    Membuat Forgot Password

--- app/src/main/java/com/example/firebaseauth/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/firebaseauth/ForgotPasswordActivity.kt
@@ -3,10 +3,12 @@ package com.example.firebaseauth
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -14,6 +16,9 @@ class ForgotPasswordActivity : AppCompatActivity() {
     //5. inisilisasi fungsi button
     private lateinit var btnSendEmail: Button
     private lateinit var tbForgotPass: androidx.appcompat.widget.Toolbar
+
+    //8. panggil fungsi button
+    private lateinit var etEmailForgotPass: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +28,32 @@ class ForgotPasswordActivity : AppCompatActivity() {
         btnSendEmail = findViewById(R.id.btnSendEmail)
         tbForgotPass = findViewById(R.id.tbForgotPass)
 
+        //8. panggil fungsi button
+        etEmailForgotPass = findViewById(R.id.etEmailForgotPass)
+
         //1. bikin fungsi action bar
         initActionBar()
 
         //3. panggil fungsi button
         btnSendEmail.setOnClickListener {
-           Toast.makeText(this, "Email sent", Toast.LENGTH_SHORT).show()
+            //8. panggil fungsi button
+            val email = etEmailForgotPass.text.toString().trim()
+
+            //9. panggil fungsi validasi input
+            if (email.isEmpty()){
+                etEmailForgotPass.error = "Please field your email"
+                etEmailForgotPass.requestFocus()
+                return@setOnClickListener
+            }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                etEmailForgotPass.error = "Please use valid email"
+                etEmailForgotPass.requestFocus()
+                return@setOnClickListener
+            }else{
+                //10. panggil fungsi forgot password
+                forgotPass(email)
+            }
+
+//           Toast.makeText(this, "Email sent", Toast.LENGTH_SHORT).show()
         }
 
         //7. panggil fungsi toolbar signup
@@ -42,5 +67,22 @@ class ForgotPasswordActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.tbForgotPass))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = ""
+    }
+
+    //10. panggil fungsi forgot password
+    private fun forgotPass(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    Toast.makeText(this, "Your reset password has been sent to your email", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, SignInActivity::class.java))
+                    finishAffinity()
+                }else{
+                    Toast.makeText(this, "Failed reset password", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            }
     }
 }
